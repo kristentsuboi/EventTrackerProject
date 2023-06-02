94908351c4a83db0fb2f54a0820c85063adebf9c
kristentsuboi <126204286+kristentsuboi@users.noreply.github.com>
2023-06-02 19:01:59 -0400

    Configure Hibernate, stub and test Vacation entity

---
+++ JPAVacationCollection/src/main/java/com/skilldistillery/vacation/entities/Vacation.java
@@ -0,0 +1,94 @@
+package com.skilldistillery.vacation.entities;
+
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Vacation {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String country;
+	
+	private String province;
+	
+	@Column(name="image_url")
+	private String imageUrl;
+	
+	private String description;
+
+	@Override
+	public String toString() {
+		return "Vacation [id=" + id + ", country=" + country + ", province=" + province + ", imageUrl=" + imageUrl
+				+ ", description=" + description + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Vacation other = (Vacation) obj;
+		return id == other.id;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getProvince() {
+		return province;
+	}
+
+	public void setProvince(String province) {
+		this.province = province;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Vacation() {
+		super();
+	}
+
+}
